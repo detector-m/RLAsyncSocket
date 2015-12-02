d8948be411eaf5d5c729585cb780ea11ffb925e0
Riven <riven@RivendeMacBook-Pro.local>
2015-12-02 20:30:39 +0800

    Initial Commit

---
+++ RLAsyncSocket/RLAsyncSocket.h
@@ -0,0 +1,19 @@
+//
+//  RLAsyncSocket.h
+//  RLAsyncSocket
+//
+//  Created by Riven on 15-12-2.
+//  Copyright (c) 2015年 Riven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RLAsyncSocket.
+FOUNDATION_EXPORT double RLAsyncSocketVersionNumber;
+
+//! Project version string for RLAsyncSocket.
+FOUNDATION_EXPORT const unsigned char RLAsyncSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RLAsyncSocket/PublicHeader.h>
+
+
